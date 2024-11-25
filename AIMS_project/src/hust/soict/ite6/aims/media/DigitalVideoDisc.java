f0336a558c9cc899cd72fc580dbedfd7bb6d5b27
F3ust <hd3876536@gmail.com>
2024-11-25 23:08:18 +0700

    refractor code

--- AIMS_project/src/hust/soict/ite6/aims/media/DigitalVideoDisc.java
+++ AIMS_project/src/hust/soict/ite6/aims/media/DigitalVideoDisc.java
@@ -1,14 +1,14 @@
 package hust.soict.ite6.aims.media;
 
 public class DigitalVideoDisc {
-    private String tile;
+    private String title;
     private String category;
     private String director;
     private int length;
     private float cost;
 
-    public String getTile() {
-        return tile;
+    public String getTitle() {
+        return title;
     }
 
     public String getCategory() {
@@ -31,27 +31,51 @@ public class DigitalVideoDisc {
 
     }
 
-    public DigitalVideoDisc(String tile, String category, String director, int length, float cost) {
-        this.tile = tile;
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        this.title = title;
         this.category = category;
         this.director = director;
         this.length = length;
         this.cost = cost;
     }
 
-    public DigitalVideoDisc(String tile, float cost) {
-        this.tile = tile;
+    public DigitalVideoDisc(String title, float cost) {
+        this.title = title;
         this.cost = cost;
     }
 
-    public DigitalVideoDisc(String tile, String category, float cost) {
-        this.tile = tile;
+    public DigitalVideoDisc(String title) {
+        this.title = title;
+    }
+
+    public DigitalVideoDisc(String title, String category, float cost) {
+        this.title = title;
         this.category = category;
         this.cost = cost;
     }
 
-    public DigitalVideoDisc(String tile, String category, String director, float cost) {
-        this.tile = tile;
+    public DigitalVideoDisc(String title, String category, String director, float cost) {
+        this.title = title;
         this.category = category;
         this.director = director;
         this.cost = cost;
