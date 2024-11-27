e1e5be19e9e93460a659b0b98af9eb91baf8ab72
F3ust <hd3876536@gmail.com>
2024-11-27 14:05:50 +0700

    add media and change book+dvd

--- AIMS_project/src/hust/soict/ite6/aims/media/DigitalVideoDisc.java
+++ AIMS_project/src/hust/soict/ite6/aims/media/DigitalVideoDisc.java
@@ -2,61 +2,29 @@ package hust.soict.ite6.aims.media;
 
 // Bui Quang Phuong 20235809
 
-public class DigitalVideoDisc {
-    private String title;
-    private String category;
+public class DigitalVideoDisc extends Media {
     private String director;
     private int length;
-    private float cost;
     private static int nbDigitalVideoDisc = 0;
-    private int id;
 
-    public String getTitle() {
-        return title;
-    }
+    public DigitalVideoDisc() {
 
-    public String getCategory() {
-        return category;
     }
 
     public String getDirector() {
         return director;
     }
 
-    public int getLength() {
-        return length;
-    }
-
-    public float getCost() {
-        return cost;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public DigitalVideoDisc() {
-
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
     public void setDirector(String director) {
         this.director = director;
     }
 
-    public void setLength(int length) {
-        this.length = length;
+    public int getLength() {
+        return length;
     }
 
-    public void setCost(float cost) {
-        this.cost = cost;
+    public void setLength(int length) {
+        this.length = length;
     }
 
     public static void setNbDigitalVideoDisc (int nbDigitalVideoDisc) {
@@ -64,45 +32,26 @@ public class DigitalVideoDisc {
     }
 
     public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
-        this.title = title;
-        this.category = category;
+        super(title, category, cost);
         this.director = director;
         this.length = length;
-        this.cost = cost;
-        ++nbDigitalVideoDisc;
-        this.id = nbDigitalVideoDisc;
     }
 
     public DigitalVideoDisc(String title, float cost) {
-        this.title = title;
-        this.cost = cost;
-        ++nbDigitalVideoDisc;
-        this.id = nbDigitalVideoDisc;
+        super(title, cost);
     }
 
     public DigitalVideoDisc(String title) {
-        this.title = title;
-        ++nbDigitalVideoDisc;
-        this.id = nbDigitalVideoDisc;
+        super(title);
     }
 
     public DigitalVideoDisc(String title, String category, float cost) {
-        this.title = title;
-        this.category = category;
-        this.cost = cost;
-        ++nbDigitalVideoDisc;
-        this.id = nbDigitalVideoDisc;
+        super(title, category, cost);
     }
 
     public DigitalVideoDisc(String title, String category, String director, float cost) {
-        this.title = title;
-        this.category = category;
+        super(title, category, cost);
         this.director = director;
-        this.cost = cost;
-        ++nbDigitalVideoDisc;
-        this.id = nbDigitalVideoDisc;
     }
-    public String toString(){
-        return String.format("DVD - %s - %s - %s - %d min(s) : %.2f $",title,category,director,length,cost);
-    }
+
 }
---
+++ AIMS_project/src/hust/soict/ite6/aims/media/Media.java
@@ -0,0 +1,76 @@
+package hust.soict.ite6.aims.media;
+
+// Bui Quang Phuong 20235809
+
+public class Media {
+    private int id;
+    private String title;
+    private String category;
+    private float cost;
+    private static int nbMedia = 0;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public void setCost(float cost) {
+        this.cost = cost;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public static void setNbMedia (int nbMedia) {
+        Media.nbMedia= nbMedia;
+    }
+
+    public Media() {
+
+    }
+
+    public Media(String title, String category, float cost) {
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+        ++nbMedia;
+        this.id = nbMedia;
+    }
+
+    public Media(String title) {
+        this.title = title;
+        ++nbMedia;
+        this.id = nbMedia;
+    }
+
+    public Media(String title, float cost) {
+        this.title = title;
+        this.cost = cost;
+        ++nbMedia;
+        this.id = nbMedia;
+    }
+
+    public String toString() {
+        return "Media - " + getId()+" - "+ getTitle() + " - " + getCategory()  + ": " + getCost() + " $";
+    }
+}
