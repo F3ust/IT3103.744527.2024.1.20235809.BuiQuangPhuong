4a4b2990abc979a8233414e5b50e3e339a7fd576
F3ust <hd3876536@gmail.com>
2024-12-18 22:40:50 +0700

    edit diagram

--- GUI_Project/src/hust/soict/ite6/javafx/Painter.java
+++ GUI_Project/src/hust/soict/ite6/javafx/Painter.java
@@ -1,4 +1,30 @@
 package hust.soict.ite6.javafx;
 
-public class Painter {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Painter extends Application {//Bui Quang Phuong 20235809
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Painter.fxml"));
+        if (root == null) {
+            System.out.println("FXML file not found!");
+        } else {
+            System.out.println("FXML file loaded successfully!");
+            Scene scene = new Scene(root);
+            stage.setTitle("Painter");
+            stage.setScene(scene);
+            stage.show();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
